1d86b0afb518c40b2334f24d5ff3b306e6f2e8ea
Uric <uricbonatti@gmail.com>
2020-07-13 12:26:52 -0300

    Add: TransactionsRepository methods, CreateTransactionService and transaction routes config

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -15,14 +21,38 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const income = incomeTransactions
+      .map(transaction => transaction.value)
+      .reduce((sumValue, value) => sumValue + value, 0);
+    const outcome = outcomeTransactions
+      .map(transaction => transaction.value)
+      .reduce((sumValue, value) => sumValue + value, 0);
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+    if (transaction.type === 'outcome') {
+      const { total } = this.getBalance();
+      if (total < value) {
+        throw Error('Outcome value exceeds cash value!');
+      }
+    }
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
